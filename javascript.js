1a163344f89584755a5540ad364dd38f304b6724
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-01-20 18:09:10 +0100

    Klar med uppgiften

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+
+function Lägg_till()
+{
+    // skapa variblen text
+    let text = document.querySelector("#ny_text")
+    // hämta texten från textrutan och spara den i varibel
+    text = ny_text.vaule;
+    // skriv ut texten i consolen för att kontrollera att det funkar
+    console.log(text.value);
+    // skapa ett li element
+    let li = document.createElement("p");
+    // sätt text i li till element till text
+    li.textContent = text.value;
+    // Hämta ul element
+    let Lista = document.querySelector("ul");
+    // lägg in li element i ul
+    Lista.appendChild(li);
+}
